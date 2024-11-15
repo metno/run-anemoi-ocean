823177d24dab631b5be9525e846340a19eb3aa62
Håvard Haugen <havardhh@met.no>
2024-11-15 15:39:35 +0200

    Makefile to set up venv

---
+++ lumi/env_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /pfs/lustrep4/$(pwd)
+
+export VIRTUAL_ENV="$(pwd)/.venv"
+if [ ! -d "$VIRTUAL_ENV" ]; then
+    mkdir -p $VIRTUAL_ENV/lib $VIRTUAL_ENV/bin
+fi
+
+export PYTHONUSERBASE=$VIRTUAL_ENV
+export PATH=$PATH:VIRTUAL_ENV/bin
+
+for repo in datasets models training graphs; do
+    if [ ! -d anemoi-$repo ]; then
+        echo "Cloning $repo"
+        git clone git@github.com:metno/anemoi-$repo.git
+    fi
+    pip install --user --no-deps -e anemoi-$repo
+done
+
+for repo in utils; do
+    if [ ! -d anemoi-$repo ]; then
+        echo "Cloning $repo"
+        git clone git@github.com:ecmwf/anemoi-$repo.git
+    fi
+    pip install --user --no-deps -e anemoi-$repo
+done
+
+   
---
+++ lumi/make_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make files executable in the container (might not be needed)
+chmod 770 env_setup.sh
+
+PROJECT_DIR=/pfs/lustrep4/scratch/project_465001383
+CONTAINER=$PROJECT_DIR/aifs/container/containers/aifs-met-pytorch-2.2.0-rocm-5.6.1-py3.9-v2.0-new-correct-anemoi-models-sort-vars.sif
+
+# Clone and pip install anemoi repos from the container
+singularity exec -B /pfs:/pfs $CONTAINER /pfs/lustrep4/$(pwd)/env_setup.sh
