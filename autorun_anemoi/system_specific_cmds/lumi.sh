2293625d04780e9090e6d33777931232e11679f1
Ina Kullmann <ina.k.kullmann@met.no>
2025-01-24 17:06:51 +0100

    not sure if this is used but updated it to make sure

--- autorun_anemoi/system_specific_cmds/lumi.sh
+++ autorun_anemoi/system_specific_cmds/lumi.sh
@@ -1,10 +1,10 @@
 PROJECT_DIR=/pfs/lustrep4/scratch/$SLURM_JOB_ACCOUNT
-CONTAINER_SCRIPT=$PROJECT_DIR/aifs/run-pytorch/run-pytorch.sh
+CONTAINER_SCRIPT=/pfs/lustrep4/projappl/$SLURM_JOB_ACCOUNT/ina/ocean-ai/run-pytorch.sh
 
 #CHANGE THESE:
-CONTAINER=$PROJECT_DIR/container/ocean-ai-trimedge.sif
+CONTAINER=$PROJECT_DIR/container/ocean-ai.sif
 
-module load LUMI/23.09 partition/G
+module load LUMI/24.03 partition/G
 
 export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
 
@@ -13,10 +13,12 @@ CPU_BIND="mask_cpu:fe000000000000,fe00000000000000,fe0000,fe000000,fe,fe00,fe000
 
 # run run-pytorch.sh in singularity container like recommended
 # in LUMI doc: https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/p/PyTorch
-srun --cpu-bind=$CPU_BIND \
+#srun --cpu-bind=$CPU_BIND \ # TODO: Use cpu bind later!
+srun \
     singularity exec -B /pfs:/pfs \
                      -B /var/spool/slurmd \
                      -B /opt/cray \
                      -B /usr/lib64 \
-                     -B /usr/lib64/libjansson.so.4 \
+                     -B /scratch/project_465001629 \
+                     -B /projappl/project_465001629 \
          $CONTAINER $CONTAINER_SCRIPT {} {}
