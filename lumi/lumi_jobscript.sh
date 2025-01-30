2a0262823aae27276a814ed148a7f7ee3bea26be
Ina Kullmann <ina.k.kullmann@met.no>
2025-01-30 16:19:32 +0200

    fixed indentation error in srun command and removed export SINGULARITYENV_LD_LIBRARY_PATH (working version!)

--- lumi/lumi_jobscript.sh
+++ lumi/lumi_jobscript.sh
@@ -4,9 +4,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --account=project_465001629
-#SBATCH --partition=dev-g
+#SBATCH --partition=standard-g
 #SBATCH --gpus-per-node=8
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --job-name=ocean-ai-mlflow
 #SBATCH --exclusive
 
@@ -21,17 +21,17 @@ VENV=$(pwd -P)/.venv # use venv installed in this dir
 export VIRTUAL_ENV=$VENV
 
 module load LUMI/24.03 partition/G
-export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
+# not needed for interconnect
+#export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
 
 # MPI + OpenMP bindings: https://docs.lumi-supercomputer.eu/runjobs/scheduled-jobs/distribution-binding
 CPU_BIND="mask_cpu:fe000000000000,fe00000000000000,fe0000,fe000000,fe,fe00,fe00000000,fe0000000000"
 
 # run run-pytorch.sh in singularity container like recommended
 # in LUMI doc: https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/p/PyTorch
-srun --cpu-bind=$CPU_BIND \ 
+srun --cpu-bind=$CPU_BIND \
     singularity exec -B /pfs:/pfs \
                      -B /var/spool/slurmd \
                      -B /opt/cray \
                      -B /usr/lib64 \
         $CONTAINER $CONTAINER_SCRIPT $CONFIG_DIR $CONFIG_NAME
-
