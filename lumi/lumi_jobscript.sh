eaf63fa440475b58296e17f91f972bf951f117e2
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-01-31 11:36:23 +0200

    revert changes to lumi_jobscript

--- lumi/lumi_jobscript.sh
+++ lumi/lumi_jobscript.sh
@@ -4,13 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --account=project_465001629
-#SBATCH --partition=standard-g
+#SBATCH --partition=dev-g
 #SBATCH --gpus-per-node=8
-#SBATCH --time=00:20:00
-#SBATCH --job-name=ocean-ai-h
+#SBATCH --time=00:10:00
+#SBATCH --job-name=ocean-ai-mlflow
 #SBATCH --exclusive
 
-CONFIG_NAME=main.yaml
+CONFIG_NAME=main.yaml 
 
 #Should not have to change these
 PROJECT_DIR=/pfs/lustrep2/scratch/$SLURM_JOB_ACCOUNT
@@ -21,19 +21,17 @@ VENV=$(pwd -P)/.venv # use venv installed in this dir
 export VIRTUAL_ENV=$VENV
 
 module load LUMI/24.03 partition/G
-# not needed for interconnect
-#export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
+export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
 
 # MPI + OpenMP bindings: https://docs.lumi-supercomputer.eu/runjobs/scheduled-jobs/distribution-binding
 CPU_BIND="mask_cpu:fe000000000000,fe00000000000000,fe0000,fe000000,fe,fe00,fe00000000,fe0000000000"
 
 # run run-pytorch.sh in singularity container like recommended
 # in LUMI doc: https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/p/PyTorch
-srun --cpu-bind=$CPU_BIND \
+srun --cpu-bind=$CPU_BIND \ 
     singularity exec -B /pfs:/pfs \
                      -B /var/spool/slurmd \
                      -B /opt/cray \
                      -B /usr/lib64 \
-                     -B /scratch/project_465001629 \
-                     -B /projappl/project_465001629 \
         $CONTAINER $CONTAINER_SCRIPT $CONFIG_DIR $CONFIG_NAME
+
