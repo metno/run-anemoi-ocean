2e035ec029dc89755b507315584a4a9bcdba3b42
Ina Kullmann <inakbk@protonmail.com>
2025-02-05 10:59:45 +0100

    Update lumi_jobscript.sh to fix NCCL timeouts

--- lumi/lumi_jobscript.sh
+++ lumi/lumi_jobscript.sh
@@ -17,22 +17,33 @@ PROJECT_DIR=/pfs/lustrep2/scratch/$SLURM_JOB_ACCOUNT
 CONTAINER_SCRIPT=$(pwd -P)/run_pytorch.sh
 chmod 770 ${CONTAINER_SCRIPT}
 CONFIG_DIR=$(pwd -P)
-CONTAINER=$PROJECT_DIR/container/ocean-ai.sif
-VENV=$(pwd -P)/.venv # use venv installed in this dir
+# NB! in order to avoid NCCL timeouts it is adviced to use 
+# pytorch 2.3.1 or above to have NCCL 2.18.3 version
+CONTAINER=$PROJECT_DIR/container/ocean-ai-pytorch-2.3.1-rocm-6.0.3-py-3.11.5-v0.0.sif
+VENV=$(pwd -P)/.venv
 export VIRTUAL_ENV=$VENV
 
 module load LUMI/24.03 partition/G
-# not needed for interconnect
-#export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
+# see https://docs.lumi-supercomputer.eu/hardware/lumig/
+# see https://docs.lumi-supercomputer.eu/runjobs/scheduled-jobs/lumig-job/
 
+# New bindings see docs above. Correct ordering of cpu affinity
+# excludes first and last core since they are not available 
+# on GPU-nodes
+CPU_BIND="mask_cpu:7e000000000000,7e00000000000000"
+CPU_BIND="${CPU_BIND},7e0000,7e000000"
+CPU_BIND="${CPU_BIND},7e,7e00"
+CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
 # MPI + OpenMP bindings: https://docs.lumi-supercomputer.eu/runjobs/scheduled-jobs/distribution-binding
 CPU_BIND="mask_cpu:fe000000000000,fe00000000000000,fe0000,fe000000,fe,fe00,fe00000000,fe0000000000"
 
 # run run-pytorch.sh in singularity container like recommended
 # in LUMI doc: https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/p/PyTorch
-srun --cpu-bind=$CPU_BIND \
-    singularity exec -B /pfs:/pfs \
+srun --cpu-bind=${CPU_BIND} \
+    singularity exec  \
+                     -B /pfs:/pfs \
                      -B /var/spool/slurmd \
                      -B /opt/cray \
                      -B /usr/lib64 \
+                     -B /opt/cray/libfabric/1.15.2.0/lib64/libfabric.so.1 \
         $CONTAINER $CONTAINER_SCRIPT $CONFIG_DIR $CONFIG_NAME
