03b582bfe2b373c1a48aa8e7663464a0da95063d
Ina Kullmann <ina.k.kullmann@met.no>
2025-05-05 12:04:46 +0300

    using newest containers with better naming

--- lumi/mlflow_sync.sh
+++ lumi/mlflow_sync.sh
@@ -3,7 +3,7 @@ PROJECT_DIR=/pfs/lustrep3/scratch/project_465001902
 SOURCE=$PROJECT_DIR/experiments/mlflow-test/logs/mlflow/ #INSERT SOURCE PATH f.ex output/logs/mlflow/
 RUNID=0b41c204fc7d409e8d4f110ae111426c # run-id of the run f.ex dca82f0b235542d1bc4e4c8fcdcb71a1
 
-CONTAINER=$PROJECT_DIR/container/ocean-ai.sif
+CONTAINER=$PROJECT_DIR/container/ocean-ai-pytorch-2.3.1-rocm-6.0.3-py-3.11.5-v0.0.sif
 export VIRTUAL_ENV="$(pwd -P)/.venv"
 
 export PYTHONUSERBASE=$VIRTUAL_ENV
