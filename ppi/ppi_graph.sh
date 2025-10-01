38015835e2a6a4c6cb125e4ef362d98ca8225cec
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-10-01 10:00:51 +0000

    graphs res 12 planar area weights

--- ppi/ppi_graph.sh
+++ ppi/ppi_graph.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name="GraphGH200"
+#SBATCH --job-name="GraphH200"
 #SBATCH --output=output/GRAPH_%j.log
-#SBATCH --gres=gpu:nvidia_gh200_480gb:1 
-#SBATCH --partition=gpuB-arm-research
-#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:nvidia_h200_nvl:2
+#SBATCH --partition=gpuB-prod
+#SBATCH --time=02:00:00
 #SBATCH --account=hi-training
-#SBATCH --mem=50g
-##SBATCH --mail-type=ALL
-##SBATCH --mail-user=mateuszm@met.no
+#SBATCH --mem=200g
+#SBATCH --ntasks-per-node=1
 
+GRAPH_NAME=trim_edge_10_res_12_MaskedPlanarAreaWeights.pt
 
-GRAPH_NAME=thinning_4_trim_edge_10_res_10.pt
-
-source /modules/rhel9/aarch64/mamba-mf3/etc/profile.d/ppimam.sh
-mamba activate /home/mateuszm/.conda/envs/gh200-p3.11.5 
+source  /modules/rhel9/x86_64/mamba-mf3/etc/profile.d/ppimam.sh
+mamba activate /home/mateuszm/.conda/envs/h200-p3.11.5
 
 VENV=$(pwd -P)/.venv
 export VIRTUAL_ENV=$VENV
@@ -26,5 +24,5 @@ export HYDRA_FULL_ERROR=1
 export AIFS_BASE_SEED=1337420
 export PYTHONUSERBASE=$VIRTUAL_ENV
 export PATH=$PATH:$VIRTUAL_ENV/bin
-
+ulimit -v unlimited
 anemoi-graphs create $CONFIG_NAME $GRAPH_NAME
