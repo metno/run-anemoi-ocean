d5c9eaff32930cf8ba05111a1f57fed587cfe845
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-10-10 09:44:02 +0000

    updated versions according to lumi changes

--- ppi/env_setup.sh
+++ ppi/env_setup.sh
@@ -14,15 +14,15 @@ if [ ! -d anemoi-core ]; then
     git clone --branch main https://github.com/ecmwf/anemoi-core.git
     cd anemoi-core
     git remote set-url origin git@github.com:ecmwf/anemoi-core.git
-    git reset --hard 801c500b00e6381b796d398db8068c25371ec5c1 # chore: Release main (#505)
+    git reset --hard 6e8bfb3589ee3fc427bbf7b5b8f3841d2c689b9a # chore: Release main (#521)
     cd ..
 fi
 
 # Install training, models, and graphs from anemoi-core
 echo "Installing training, models, and graphs from anemoi-core"
-pip install --user -e anemoi-core/training
-pip install --user -e anemoi-core/models
-pip install --user -e anemoi-core/graphs
+pip install --user --no-deps -e anemoi-core/training
+pip install --user --no-deps -e anemoi-core/models
+pip install --user --no-deps -e anemoi-core/graphs
 
 # Clone and install utils if not already cloned
 if [ ! -d anemoi-utils ]; then
@@ -32,7 +32,7 @@ if [ ! -d anemoi-utils ]; then
     git remote set-url origin git@github.com:ecmwf/anemoi-utils.git
     cd ..
 fi
-pip install --user -e anemoi-utils
+pip install --user --no-deps -e anemoi-utils
 
 # Clone and install datasets if not already cloned
 if [ ! -d anemoi-datasets ]; then
@@ -44,7 +44,7 @@ if [ ! -d anemoi-datasets ]; then
     git reset --hard 83bb7262238fd8c20f0b6fdcf75d67bcfdb2d1db # chore(main): Release 0.5.26 (#343)
     cd ..
 fi
-pip install --user -e anemoi-datasets
+pip install --user --no-deps -e anemoi-datasets
 
 # Get the mlflow package to do offline sync
 # Recomended way to install from README on GitHub
@@ -58,8 +58,22 @@ if [ ! -d anemoi-transform ]; then
     cd ..
 fi 
 
-pip install --user -e anemoi-transform
+pip install --user --no-deps -e anemoi-transform
 
-cp lam.yaml ./anemoi-core/training/src/anemoi/training/config/training/scalers/
-cat ./anemoi-core/training/src/anemoi/training/config/training/scalers/lam.yaml | grep tendency
-cp graphtransformer.yaml ./anemoi-core/training/src/anemoi/training/config/model/
+if [ ! -d anemoi-inference ]; then
+    echo "Cloning anemoi-inference from ecmwf"
+    git clone https://github.com/ecmwf/anemoi-inference.git
+    cd anemoi-inference
+    git remote set-url origin git@github.com:ecmwf/anemoi-inference.git
+    git reset --hard c02c45aa1329fdc0c660ee2d7eccac2b15bc0514 # working version with current runner.py
+    echo "Copying runner.py to anemoi-inference/src/anemoi/inference/ to avoid cuda-bug"
+    cp ../runner.py ./src/anemoi/inference/
+    cat ./src/anemoi/inference/runner.py | grep cuda
+    cd ..
+fi
+
+#echo "Installing anemoi-inference"
+pip install --user -e anemoi-inference # consider adding dependencies to container later
+
+echo "Did the runner.py copy work?"
+cat ./anemoi-inference/src/anemoi/inference/runner.py | grep cuda
