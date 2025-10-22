5ef4350d537e72168866590aadf5d55489725cc0
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-10-22 13:58:55 +0000

    removed some comments

--- ppi/external_checkpoint_inference/ppi_infer.sh
+++ ppi/external_checkpoint_inference/ppi_infer.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name="Infer"
 #SBATCH --output=output/Infer_%j.log
-#SBATCH --gres=gpu:nvidia_h200_nvl:2
+#SBATCH --gres=gpu:nvidia_h200_nvl:1
 #SBATCH --partition=gpuB-prod
 #SBATCH --time=00:30:00
 #SBATCH --account=hi-training
 #SBATCH --mem=80G
 #SBATCH --ntasks-per-node=1
 
-
-#source  /modules/rhel9/x86_64/mamba-mf3/etc/profile.d/ppimam.sh
-#mamba activate /modules/rhel9/x86_64/mamba-mf3/envs/2025-08-development
-
-#VENV=$(pwd -P)/.venv
-#export VIRTUAL_ENV=$VENV
-
-
 source $(pwd -P)/.venv/bin/activate
 
 CONFIG_DIR=$(pwd -P)/
-CONFIG_NAME=infer.yaml
+CONFIG_NAME=$CONFIG_DIR/infer.yaml
 
 export HYDRA_FULL_ERROR=1
 export AIFS_BASE_SEED=1337420
@@ -27,3 +19,4 @@ export AIFS_BASE_SEED=1337420
 ulimit -v unlimited
 anemoi-inference run $CONFIG_NAME
 
+python ../postpro-inference.py $CONFIG_NAME 
