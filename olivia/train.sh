97238a5398bdf70d41a90eb4b3384682c15f8e18
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-11-13 10:35:35 +0100

    working train.sh

--- olivia/train.sh
+++ olivia/train.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 #SBATCH --account=nn12017k
-#SBATCH --job-name=train_4gpu
+#SBATCH --job-name=train
 #SBATCH --partition=accel
 #SBATCH --nodes=1
-#SBATCH --gpus=4
+#SBATCH --gpus=1
 #SBATCH --cpus-per-task=72
 #SBATCH --mem=0
 #SBATCH --time=00:10:00
-#SBATCH --output=outputs/train_4gpu_%j.out
+#SBATCH --output=outputs/train_%j.out
 
 echo "Multi-GPU Training Test (DDP with NCCL)"
 echo ""
 
 SIF=/cluster/projects/nn12017k/container/pytorch_25.08-py3.sif
-SQSH=./anemoi-env.sqsh
+SQSH=$PWD/anemoi-env.sqsh
 export APPTAINERENV_PREPEND_PATH=/user-software/bin
-CONFIG_NAME=$PWD/main-core.yaml
+CONFIG_NAME=main-core.yaml
+echo $CONFIG_NAME
 
-apptainer exec --nv -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
-    bash -c "source /user-software/bin/activate && anemoi-training train --config-name=$CONFIG_NAME"
+apptainer exec --nv -B /cluster/projects/nn12017k/ -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
+    bash -c "source /user-software/bin/activate && python -m anemoi.training train --config-dir=$PWD --config-name=$CONFIG_NAME"
 
+#apptainer exec --nv -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
+#    bash -c "source /user-software/bin/activate && python train_single_gpu.py"
 # bash -c "source /user-software/bin/activate && torchrun --standalone --nproc_per_node=4 train_multi_gpu.py"
