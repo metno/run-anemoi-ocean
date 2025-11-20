1cd8ba03031db50ff8b8ff94e053a03b229fe77a
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2025-11-20 14:50:00 +0100

    added bind to data location

--- olivia/train.sh
+++ olivia/train.sh
@@ -15,10 +15,10 @@ echo ""
 SIF=/cluster/projects/nn12017k/container/pytorch_25.08-py3.sif
 SQSH=$PWD/anemoi-env.sqsh
 export APPTAINERENV_PREPEND_PATH=/user-software/bin
-CONFIG_NAME=main-core.yaml
+CONFIG_NAME=SF_main.yaml
 echo $CONFIG_NAME
 
-apptainer exec --nv -B /cluster/projects/nn12017k/ -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
+apptainer exec --nv -B /cluster/work/projects/nn12017k/ -B /cluster/projects/nn12017k/ -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
     bash -c "source /user-software/bin/activate && python -m anemoi.training train --config-dir=$PWD --config-name=$CONFIG_NAME"
 
 #apptainer exec --nv -B $PWD -B ${SQSH}:/user-software:image-src=/ ${SIF} \
